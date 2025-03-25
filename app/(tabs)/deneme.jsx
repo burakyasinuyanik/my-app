d3ef8d846c9acd5302287a0eff108fd358aeb60f
burakyasinuyanik <116893884+burakyasinuyanik@users.noreply.github.com>
2025-03-26 02:00:46 +0300

    calismalara devam

--- app/(tabs)/deneme.jsx
+++ app/(tabs)/deneme.jsx
@@ -1,12 +1,32 @@
-import { StyleSheet, Image, Platform, View } from 'react-native';
+import { StyleSheet, Image, Text, View, TextInput ,ScrollView} from 'react-native';
 import React from 'react';
+import Welcome from '../components/welcome';
 
 
 const deneme=()=>{
 
     return (
-        <view type='title' style={{color:"white",backgroundColor:"lightblue"}}>Merhaba</view>
-    )
+        <ScrollView>
+          <Text>Some text</Text>
+          <View>
+            <Text>Some more text</Text>
+            <Image
+              source={{
+                uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+              }}
+              style={{width: 200, height: 200}}
+            />
+          </View>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+            }}
+            defaultValue="You can type in me"
+          />
+        </ScrollView>
+      );
 }
 export default deneme
 const styles = StyleSheet.create({
---
+++ app/components/welcome.jsx
@@ -0,0 +1,13 @@
+import {StyleSheet,Text,TextInput,View} from 'react-native'
+import React    from 'react'
+const Welcome=()=>{
+    return (
+        <View>
+            <Text>React Native Dersleri component olayıdır.</Text>
+           
+        </View>
+    )
+}
+export default Welcome
+
+const styles =StyleSheet.create({})
